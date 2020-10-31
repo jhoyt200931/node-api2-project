b638c3dc29b3fc834cddc0ca11ba3d988e1b8dc4
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-10-31 00:01:29 -0400

    finished endpoints

--- expressRouter.js
+++ expressRouter.js
@@ -15,7 +15,7 @@ function validatePost(post) {
 
 router.post('/', (req, res) => {
     const post = req.body;
-
+    const id = null;
     if(validatePost(post)) {
         db.insert(post)
             .then(id => {
@@ -45,6 +45,14 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(post => {
+            res.status(200).json(post);
+        });
+});
+
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     db.remove(id)
@@ -59,6 +67,95 @@ router.delete('/:id', (req, res) => {
             console.log(error);
             res.status(500).json({ error: "The post could not be removed" })
         })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    if(validatePost(changes)){
+        db.update(id, changes)
+            .then(post => {
+                if(post > 0){
+                    res.status(200).json(post);
+                } else{
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ error: "The post information could not be modified." })
+            })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+
+})
+
+router.post('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    const comment = req.body;
+    let postId = null;
+    function validateComment(param) {
+        if(!param.text){
+            return false
+        } else {
+            return true
+        }
+    }
+
+    if(validateComment(comment)) {
+        db.findById(id)
+            .then(post => {
+                console.log(post);
+                postId = post[0].id;
+                if(postId){
+                    db.insertComment(comment)
+                                .then(newComment => {
+                                    res.status(201).json(newComment);
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                    res.status(500).json({ error: "There was an error while saving the comment to the database" })
+                                })
+                } else {
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+            })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    }
+})
+
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    let postId = null;
+
+    db.findById(id)
+            .then(post => {
+                console.log(post);
+                postId = post[0].id;
+                if(postId){
+                    db.findPostComments(postId)
+                        .then(comments => {
+                            res.status(200).json(comments)
+                        })
+                        .catch(error => {
+                            console.log(error);
+                            res.status(500).json({ error: "The comments information could not be retrieved." })
+                        })
+                } else {
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ error: "The comments information could not be retrieved." })
+            })
 })
 
 module.exports = router;
